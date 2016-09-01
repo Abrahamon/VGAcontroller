f4431605860c2ec7766ae8136381ea25595ff6e4
Abraham Arias Chinchilla <Abraham Arias Chinchilla>
2016-09-01 11:02:55 -0600

    modulo principal

---
+++ VGA_Top.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:35:08 09/01/2016 
+// Design Name: 
+// Module Name:    VGA_Top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module VGA_Top(
+    input a,
+    output R,G,B,H_SYNC, V_SYNC
+	 );
+
+
+endmodule
